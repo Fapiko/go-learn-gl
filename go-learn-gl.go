4eb0dd854f6e1a51b96fb77e9d2e23885274a7ab
Lucas Jandrew <ljandrew@riotgames.com>
2015-02-15 00:44:50 -0500

    Initial commit

---
+++ go-learn-gl.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/go-gl/gl"
+	glfw "github.com/go-gl/glfw3"
+	//	"time"
+)
+
+func main() {
+	if !glfw.Init() {
+		log.Fatal("Failed to initialize GLFW")
+
+		os.Exit(-1)
+	}
+
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Samples, 4)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenglForwardCompatible, glfw.True)
+	glfw.WindowHint(glfw.OpenglProfile, glfw.OpenglCoreProfile)
+
+	window, error := glfw.CreateWindow(1024, 768, "Tutorial 01", nil, nil)
+
+	if error != nil {
+		log.Fatal("Failed to open GLFW window.")
+		glfw.Terminate()
+		os.Exit(-1)
+	}
+
+	window.MakeContextCurrent()
+
+	gl.Init()
+
+	//	for true {
+	//		time.Sleep(100 * time.Millisecond)
+	//	}
+}
