14ce4700c89ef0d5125bef42bf432c681caf2d46
Lucas Jandrew <fapiko@gmail.com>
2016-02-07 04:28:41 -0500

    Adding tutorial 5

---
+++ 05-a-textured-cube/main.go
@@ -0,0 +1,396 @@
+package main
+
+import (
+	"errors"
+	"io/ioutil"
+
+	log "github.com/Sirupsen/logrus"
+
+	"strings"
+
+	"os"
+
+	"encoding/binary"
+
+	"github.com/go-gl/gl/all-core/gl"
+	"github.com/go-gl/glfw3/v3.1/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+// Tutorial 04 - A colored cube ported from
+// http://www.opengl-tutorial.org/beginners-tutorials/tutorial-2-the-first-triangle/
+func main() {
+
+	if err := glfw.Init(); err != nil {
+		panic("Failed to initialize GLFW")
+	}
+
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Samples, 4)
+
+	// Drawing the triangle threw an error with OpenGL 3.3, downgrading to 2.1 seemed to solve it
+	glfw.WindowHint(glfw.ContextVersionMajor, 2)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+
+	// Open a window and create its OpenGL context
+	window, err := glfw.CreateWindow(1024, 768, "Tutorial 04", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	window.MakeContextCurrent()
+
+	// Initialize OpenGL - Go bindings use Glow and now Glew
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	window.SetInputMode(glfw.StickyKeysMode, gl.TRUE)
+
+	// Dark blue background
+	gl.ClearColor(0.0, 0.0, 0.4, 0.0)
+
+	// Enable depth test
+	gl.Enable(gl.DEPTH_TEST)
+
+	// Accept fragment if it is closer to the camera than the former one
+	gl.DepthFunc(gl.LESS)
+
+	// Create and compile our GLSL program from the shaders
+	programId := loadShaders("TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader")
+	defer gl.DeleteProgram(programId)
+
+	// Get a handle for our "MVP" uniform
+	matrixId := gl.GetUniformLocation(programId, gl.Str("MVP\x00"))
+
+	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	projection := mgl32.Perspective(45.0, 4.0/3.0, 0.1, 100.0)
+	// Or, for an ortho camera :
+	// projection := mgl32.Ortho(-10.0, 10.0, -10.0, 10.0, 0.0, 100.0) // In world coordinates
+
+	// Camera matrix
+	view := mgl32.LookAt(4.0, 3.0, -3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+
+	// Model matrix : an identity matrix (model will be at the origin)
+	model := mgl32.Ident4()
+
+	mvp := projection.Mul4(view).Mul4(model)
+
+	// Get a handle for our buffers
+	vertexPositionModelspaceId := uint32(gl.GetAttribLocation(programId, gl.Str("vertexPosition_modelspace\x00")))
+
+	vertexBufferData := []float32{
+		-1.0, -1.0, -1.0, // triangle 1 : begin
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0, // triangle 1 : end
+		1.0, 1.0, -1.0, // triangle 2 : begin
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0, // triangle 2 : end
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+	}
+
+	uvBufferData := []float32{
+		0.000059, 1.0 - 0.000004,
+		0.000103, 1.0 - 0.336048,
+		0.335973, 1.0 - 0.335903,
+		1.000023, 1.0 - 0.000013,
+		0.667979, 1.0 - 0.335851,
+		0.999958, 1.0 - 0.336064,
+		0.667979, 1.0 - 0.335851,
+		0.336024, 1.0 - 0.671877,
+		0.667969, 1.0 - 0.671889,
+		1.000023, 1.0 - 0.000013,
+		0.668104, 1.0 - 0.000013,
+		0.667979, 1.0 - 0.335851,
+		0.000059, 1.0 - 0.000004,
+		0.335973, 1.0 - 0.335903,
+		0.336098, 1.0 - 0.000071,
+		0.667979, 1.0 - 0.335851,
+		0.335973, 1.0 - 0.335903,
+		0.336024, 1.0 - 0.671877,
+		1.000004, 1.0 - 0.671847,
+		0.999958, 1.0 - 0.336064,
+		0.667979, 1.0 - 0.335851,
+		0.668104, 1.0 - 0.000013,
+		0.335973, 1.0 - 0.335903,
+		0.667979, 1.0 - 0.335851,
+		0.335973, 1.0 - 0.335903,
+		0.668104, 1.0 - 0.000013,
+		0.336098, 1.0 - 0.000071,
+		0.000103, 1.0 - 0.336048,
+		0.000004, 1.0 - 0.671870,
+		0.336024, 1.0 - 0.671877,
+		0.000103, 1.0 - 0.336048,
+		0.336024, 1.0 - 0.671877,
+		0.335973, 1.0 - 0.335903,
+		0.667969, 1.0 - 0.671889,
+		1.000004, 1.0 - 0.671847,
+		0.667979, 1.0 - 0.335851,
+	}
+
+	var vertexBuffer uint32
+	gl.GenBuffers(1, &vertexBuffer)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertexBufferData)*4, gl.Ptr(vertexBufferData), gl.STATIC_DRAW)
+	defer gl.DeleteBuffers(1, &vertexBuffer)
+
+	_, err = loadBmpCustom("uvtemplate.bmp")
+	if err != nil {
+		panic(err)
+	}
+
+	var textureBuffer uint32
+	gl.GenBuffers(1, &textureBuffer)
+	gl.BindBuffer(gl.TEXTURE_BUFFER, textureBuffer)
+	gl.BufferData(gl.TEXTURE_BUFFER, len(uvBufferData)*4, gl.Ptr(uvBufferData), gl.STATIC_DRAW)
+	defer gl.DeleteBuffers(1, &textureBuffer)
+
+	for window.GetKey(glfw.KeyEscape) != glfw.Press && !window.ShouldClose() {
+
+		// Clear the screen
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		// Use our shader
+		gl.UseProgram(programId)
+
+		// Send our transformation to the currently bound shader, in the "MVP" uniform
+		gl.UniformMatrix4fv(matrixId, 1, false, &mvp[0])
+
+		// 1st attribute buffer : vertices
+		gl.EnableVertexAttribArray(vertexPositionModelspaceId)
+		gl.BindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+		gl.VertexAttribPointer(
+			vertexPositionModelspaceId, // The attribute we want to configure
+			3,               // size
+			gl.FLOAT,        // type
+			false,           // normalized?
+			0,               // stride
+			gl.PtrOffset(0)) // array buffer offset
+
+		// Draw the triangle !
+		gl.DrawArrays(gl.TRIANGLES, 0, 12*3) // 12*3 indices starting at 0 -> 12 triangles
+		gl.DisableVertexAttribArray(vertexPositionModelspaceId)
+
+		// 2nd attribute buffer : colors
+		gl.EnableVertexAttribArray(1)
+		gl.BindBuffer(gl.ARRAY_BUFFER, textureBuffer)
+		gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 0, nil)
+
+		// Swap buffers
+		window.SwapBuffers()
+		glfw.PollEvents()
+
+	}
+
+}
+
+func loadShaders(vertexFilePath string, fragmentFilePath string) uint32 {
+
+	vertexShaderId := gl.CreateShader(gl.VERTEX_SHADER)
+	defer gl.DeleteShader(vertexShaderId)
+
+	fragmentShaderId := gl.CreateShader(gl.FRAGMENT_SHADER)
+	defer gl.DeleteShader(fragmentShaderId)
+
+	// Read the Vertex Shader code from the file
+	vertexShaderCode, err := ioutil.ReadFile(vertexFilePath)
+	if err != nil {
+
+		log.Errorf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !",
+			vertexFilePath)
+		return 0
+
+	}
+
+	// Read the Fragment Shader code from the file
+	fragmentShaderCode, err := ioutil.ReadFile(fragmentFilePath)
+	if err != nil {
+
+		log.Errorf("Impossible to open %s. Are you in the right directory ? Don't forget to read the FAQ !",
+			fragmentFilePath)
+		return 0
+
+	}
+
+	// Compile Vertex Shader
+	log.Infof("Compiling shader : %s", vertexFilePath)
+	vertexSourcePointer := gl.Str(nullTerminatedString(vertexShaderCode))
+
+	gl.ShaderSource(vertexShaderId, 1, &vertexSourcePointer, nil)
+	gl.CompileShader(vertexShaderId)
+
+	// Check Vertex Shader
+	var infoLogLength int32
+	var result int32
+
+	gl.GetShaderiv(vertexShaderId, gl.COMPILE_STATUS, &result)
+	gl.GetShaderiv(vertexShaderId, gl.INFO_LOG_LENGTH, &infoLogLength)
+	if result != gl.TRUE {
+
+		vertexShaderErrorMessage := strings.Repeat("\x00", int(infoLogLength))
+
+		var messageLength int32
+		gl.GetShaderInfoLog(vertexShaderId, infoLogLength, &messageLength, gl.Str(vertexShaderErrorMessage))
+
+		log.Info(vertexShaderErrorMessage)
+
+	}
+
+	// Compile Fragment Shader
+	log.Infof("Compiling shader : %s", fragmentFilePath)
+	fragmentSourcePointer := gl.Str(nullTerminatedString(fragmentShaderCode))
+
+	gl.ShaderSource(fragmentShaderId, 1, &fragmentSourcePointer, nil)
+	gl.CompileShader(fragmentShaderId)
+
+	gl.GetShaderiv(fragmentShaderId, gl.COMPILE_STATUS, &result)
+	gl.GetShaderiv(fragmentShaderId, gl.INFO_LOG_LENGTH, &infoLogLength)
+	if result != gl.TRUE {
+
+		fragmentShaderErrorMessage := strings.Repeat("\x00", int(infoLogLength))
+
+		var messageLength int32
+		gl.GetShaderInfoLog(vertexShaderId, infoLogLength, &messageLength, gl.Str(fragmentShaderErrorMessage))
+
+		log.Info(fragmentShaderErrorMessage)
+
+	}
+
+	// Link the program
+	log.Printf("Linking program")
+	programId := gl.CreateProgram()
+	gl.AttachShader(programId, vertexShaderId)
+	gl.AttachShader(programId, fragmentShaderId)
+	gl.LinkProgram(programId)
+
+	gl.GetProgramiv(programId, gl.LINK_STATUS, &result)
+	gl.GetProgramiv(programId, gl.INFO_LOG_LENGTH, &infoLogLength)
+	if result != gl.TRUE {
+
+		programErrorMessage := strings.Repeat("\x00", int(infoLogLength))
+
+		var messageLength int32
+		gl.GetProgramInfoLog(programId, infoLogLength, &messageLength, gl.Str(programErrorMessage))
+
+		log.Info(programErrorMessage)
+
+	}
+
+	defer gl.DetachShader(programId, vertexShaderId)
+	defer gl.DetachShader(programId, fragmentShaderId)
+
+	return programId
+
+}
+
+func nullTerminatedString(source []byte) string {
+	return string(append(source, make([]byte, 1)...))
+}
+
+func loadBmpCustom(filepath string) (uint32, error) {
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		return 0, errors.New("Image could not be opened")
+	}
+	defer file.Close()
+
+	//bufferedReader := bufio.NewReader(file)
+	header := make([]byte, 54)
+
+	_, err = file.Read(header)
+	if err != nil {
+		return 0, errors.New("Not a correct bmp file")
+	}
+
+	if header[0] != 'B' || header[1] != 'M' {
+		return 0, errors.New("Not a correct bmp file")
+	}
+
+	// Read ints from the byte array
+	dataPos := intFromByteSlice(header[0x0A : 0x0A+4])
+	imageSize := intFromByteSlice(header[0x22 : 0x22+4])
+	width := intFromByteSlice(header[0x12 : 0x12+4])
+	height := intFromByteSlice(header[0x16 : 0x16+4])
+
+	// Some BMP files are misformatted, guess missing information
+	if imageSize == 0 {
+		imageSize = width * height * 3 // 3 : one byte for each Red, Green and Blue component
+	}
+
+	if dataPos == 0 {
+		dataPos = 54 // The BMP header is done that way
+	}
+
+	// Create a buffer
+	data := make([]byte, 786432)
+
+	// Read the actual data from the file into the buffer
+	_, err = file.Read(data)
+	if err != nil {
+		return 0, errors.New("bmp smaller than expected")
+	}
+
+	// Create one OpenGL texture
+	var textureId uint32
+	gl.GenTextures(1, &textureId)
+
+	// "Bind" the newly created texture : all future texture functions will modify this texture
+	gl.BindTexture(gl.TEXTURE_2D, textureId)
+
+	// Give the image to OpenGL
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGB, int32(width), int32(height), 0, gl.BGR, gl.UNSIGNED_BYTE, gl.Ptr(data))
+
+	// Poor filter, or ...
+	//gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+	//gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+
+	// ... nice trilinear filtering
+	gl.TexParameterf(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+	// When MAGnifying the image (no bigger mipmap available), use LINEAR filtering
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	// When MINifying the image, use a LINEAR blend of two mipmaps, each filtered LINEARLY too
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR)
+	// Generate mipmaps, by the way.
+	gl.GenerateMipmap(gl.TEXTURE_2D)
+
+	return textureId, nil
+
+}
+
+func intFromByteSlice(bytes []byte) int {
+	return int(binary.LittleEndian.Uint32(bytes))
+}
